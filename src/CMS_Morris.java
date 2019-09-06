676e4892978ec472a99a76edf600427beb9dac52
Xiuge Chen <xiugec@student.unimelb.edu.au>
2019-09-06 15:38:20 +1000

    finish implementation

--- src/CMS_Morris.java
+++ src/CMS_Morris.java
@@ -1,26 +1,45 @@
 // COMP90056 Assignment A 2019s2
 
-// write your name/login here
+// xiugec 961392
+// Xiuge Chen
+// xiugec@student.unimelb.edu.au
 
-public class CMS_Morris implements CMS
-{
-
+public class CMS_Morris implements CMS {
 	int width;
 	int depth;
+	private Hash h[];
+	private Morris c_morris[][];
 
-	CMS_Morris(int d, int w)
-	{
-		// finish
+	CMS_Morris(int d, int w) {
+		width = w;
+		depth = d;
+		h = new Hash[depth];
+		c_morris = new Morris[depth][width];
+		
+		for (int j =0; j<depth; ++j) {
+			h[j] = new Hash();
+		}
 	}
 
-	public void update(Object o, int freq)
-	{
-		// finish
+	public void update(Object o, int freq) {	
+		if (freq > 0) {
+			for (int j = 0; j < depth; j++) {
+				c_morris[j][h[j].h_basic(o, width)].increment(freq);
+			}
+		}
 	}
 
-	public int query(Object o)
-	{
-		// finish
-		return 0;
+	public int query(Object o) {
+		int min = c_morris[0][h[0].h_basic(o, width)].mycount();
+		int current;
+		
+		for (int j=1; j < depth ; j++) {
+			current = c_morris[j][h[j].h_basic(o, width)].mycount();
+			
+			if(current < min) {
+				min = current;
+			}
+		}
+		return min;
 	}
 }
--- src/CMS_conservative.java
+++ src/CMS_conservative.java
@@ -1,25 +1,47 @@
 // COMP90056 Assignment A 2019s2
 
-// xiugec 961392
-// Xiuge Chen
-// xiugec@student.unimelb.edu.au
+// if you edit this file, write your name/login here
 
 public class CMS_conservative implements CMS {
-
-	int width;
-	int depth;
+	private int width;
+	private int depth;
+	private Hash h[];
+	private int c[][];
 
 	CMS_conservative(int d, int w) {
-		// finish
+		width = w;
+		depth = d;
+		h = new Hash[depth];
+		c = new int[depth][width];
+		
+		for (int j =0; j<depth; ++j) {
+			h[j] = new Hash();
+		}
 	}
 
-	public void update(Object o, int freq)
-	{
-		// finish
+	public void update(Object o, int freq) {
+		Integer f_e = query(o);
+		
+		for (int j = 0; j < depth; j++) {
+			int col = h[j].h_basic(o, width);
+			
+			if (f_e + freq > c[j][col]) {
+				c[j][col] = f_e + freq;
+			}
+		}
 	}
 
-	public int query(Object o)
-	{
-		return 0;
+	public int query(Object o) {
+		int min = c[0][h[0].h_basic(o, width)];
+		int current;
+		
+		for (int j=1; j < depth ; j++) {
+			current = c[j][h[j].h_basic(o, width)];
+			
+			if(current < min) {
+				min = current;
+			}
+		}
+		return min;
 	}
 }
